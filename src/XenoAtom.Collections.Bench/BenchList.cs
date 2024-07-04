4f31175a74d66f4ac04f252d7bb2706e7ba8f2a4
Alexandre Mutel <alexandre_mutel@live.com>
2024-07-04 23:07:16 +0200

    Update benchmarks

--- src/XenoAtom.Collections.Bench/BenchList.cs
+++ src/XenoAtom.Collections.Bench/BenchList.cs
@@ -9,11 +9,13 @@ namespace XenoAtom.Collections.Bench;
 public partial class BenchList
 {
     private UnsafeList<int> _unsafeList;
+    private UnsafeList<int>.N16 _unsafeList16;
     private readonly List<int> _list;
 
     public BenchList()
     {
         _unsafeList = new();
+        _unsafeList16 = new UnsafeList<int>.N16();
         _list = new();
     }
 
@@ -33,6 +35,22 @@ public partial class BenchList
         list.Add(10);
     }
 
+    [Benchmark(Description = "UnsafeList<int>.N16.Add")]
+    public void UnsafeListN16()
+    {
+        ref var list = ref _unsafeList16;
+        list.Clear();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+        list.Add(6);
+        list.Add(7);
+        list.Add(8);
+        list.Add(10);
+    }
+    
     [Benchmark(Description = "List<int>.Add", Baseline = true)]
     public void List()
     {
